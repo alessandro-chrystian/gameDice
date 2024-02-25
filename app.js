f8d640d8551d2aa62d69c57482165fe2d72f0fae
Alessandro Chrystian <alessandroc.dev@gmail.com>
2024-02-25 20:16:27 -0300

    Versão 2.0 com botão para jogar.

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-let randomNumber1 = Math.floor(Math.random() * 6) + 1
+let btn = document.querySelector('.rollDices')
+let resultAnnouncer = document.querySelector('h1')
+let diceImage = document.querySelectorAll('img')
 
-let randomImageSource1 = 'img/dice' + randomNumber1 + '.png' // dice1.png
+btn.addEventListener('click', function() {
+    let randomNumber1 = Number(Math.floor(Math.random() * 6 + 1))
+    let randomNumber2 = Number(Math.floor(Math.random() * 6 + 1))
+    
+    diceImage[0].setAttribute('src', 'img/dice' + randomNumber1 + '.png')
+    diceImage[1].setAttribute('src', 'img/dice' + randomNumber2 + '.png')
+    
 
-let image1 = document.querySelectorAll('img')[0].setAttribute('src', randomImageSource1)
-
-let randomNumber2 = Math.floor(Math.random() * 6) + 1
-
-let randomImageSource2 = 'img/dice' + randomNumber2 + '.png'
-
-document.querySelectorAll('img')[1].setAttribute('src', randomImageSource2)
-
-if( randomNumber1 > randomNumber2) {
-    document.querySelector('h1').innerHTML = '🏆 Jogador 1 ganhou'
-} else if(randomNumber2 > randomNumber1) {
-    document.querySelector('h1').innerHTML = '🏆 Jogador 2 ganhou'
-} else {
-    document.querySelector('h1').innerHTML = 'Empate'
-}
+    if(randomNumber1 > randomNumber2) {
+        resultAnnouncer.innerHTML = 'Jogador 1 venceu!'
+    } else if(randomNumber2 > randomNumber1) {
+        resultAnnouncer.innerHTML = 'Jogador 2 venceu!'
+    } else {
+        resultAnnouncer.innerHTML = 'Empate!'
+    }
+})
